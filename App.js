577bafebb8432e632715f8015641b389c0c97371
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2025-08-31 22:16:32 +0100

    feat: implement Phase 1 - HTML to React progression

---
+++ App.js
@@ -0,0 +1,3 @@
+ const heading = React.createElement("h1",{id:"heading"},"Hello from React World")
+        const root = ReactDOM.createRoot(document.getElementById("root"))
+        const Root = root.render(heading)
