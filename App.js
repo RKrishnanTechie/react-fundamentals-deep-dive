f6368b26cc24b8d05ab51cf9a078f157478b30c9
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2025-09-03 18:32:11 +0100

    Header component skeleton

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img
+          className="img"
+          src="https://static.vecteezy.com/system/resources/previews/048/067/082/non_2x/biryani-dish-malabar-biryani-with-thalassery-chicken-isolated-illustration-on-a-transparent-background-png.png"
+        />
+        <h3 className="brand-name"> Lets Eat</h3>
+      </div>
+      <div className="nav">
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 const AppComponent = () => {
   return (
     <div>
-      <h1>Hello App comp</h1>
+      <HeaderComponent />
     </div>
   );
 };
