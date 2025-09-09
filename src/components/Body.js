d44fc8e0e55047963830449d7781561c287aa0e7
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2025-09-09 11:27:40 +0100

    removed Mock data and generating dynamic Cards by fetching API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,30 @@
 import CardComponent from "./Card";
-import restaurantList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const BodyComponent = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList); // State variable to hold restaurant data
+    const [listOfRestaurants, setListOfRestaurants] = useState([]); // State variable to hold restaurant data
   
+    useEffect(() => {
+     fetchData();
+    },[])
+
+    const fetchData = async () => {
+        const data = await fetch("https://swiggy-api-4c740.web.app/swiggy-api.json");
+        const json = await data.json();
+        console.log(json);      
+        setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []); // Update this path after checkiing JSON Viewer Pro extension in your browser
+    }
+
+    if(listOfRestaurants.length === 0){
+        return <h1>Loading...</h1>
+    }
+
     return (
     <div className="body">
     <div className="filter-data">
       <button className="btn-filter" onClick={() => {
         const filteredList = listOfRestaurants.filter(
-          (res) => res.rating > 4
+          (res) => Number(res.info.avgRating) > 4.3
         );
         setListOfRestaurants(filteredList);
       }}>
@@ -19,7 +33,7 @@ const BodyComponent = () => {
     </div>
       <div className="restaurant-list">
         {listOfRestaurants.map((restaurant) => (
-          <CardComponent key={restaurant.id} resData={restaurant} />
+          <CardComponent key={restaurant.info.id} resData={restaurant.info} />
         ))}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,15 @@
 const CardComponent = ({ resData }) => {
-  const { name, image, cuisine, rating } = resData; // resData?.data if API data
+  const { name, cloudinaryImageId, cuisines, avgRating, sla } = resData; // resData?.data if API data
   return (
     <div className="card">
-      <img className="card-img" src={image} />
+      <img className="card-img" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + 
+        cloudinaryImageId}
+       />
       <h3>{name}</h3>
-      <h4> {cuisine}</h4>
-      <h4>{rating} Stars</h4>
-      <button>Order Now</button>
+      <h4> {cuisines?.join(",")}</h4>
+      <h4>{avgRating} Stars</h4>
+      <h4>{sla?.deliveryTime} Minutes</h4>
+      <button> Order Now</button>
     </div>
   );
 };
